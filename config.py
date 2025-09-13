c5d396862d5b4dbb12147159eeee3894d76e8ab6
sieunhandienquang69 <trongkhangnguyen503@gmail.com>
2025-09-13 11:04:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # File này chứa các thông tin bí mật
-EMAIL_USER = 'trongkhangnguyen503@gmail.com'
-EMAIL_PASS = 'fyjp impk mpdl rjtc' # Mật khẩu ứng dụng 16 ký tự
+EMAIL_USER = 'webngt11@gmail.com'
+EMAIL_PASS = 'fzzv rumf xvtu mylg' # Mật khẩu ứng dụng 16 ký tự
 SECRET_KEY = '2b5d2f60a8d2be48e34019fd891d1a85'
